d2d226c0b60a2de902667b14002cb3cf2cdb86e0
Ambesawi <ambesawi@gmail.com>
2023-02-22 14:08:22 -0800

    final

--- shell_loop.c
+++ shell_loop.c
@@ -48,9 +48,9 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * 2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
